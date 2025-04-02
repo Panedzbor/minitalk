a0ce9337854a806772b0e975c3dde4eefa133d8e
Edgar Arutiunian <earutiun@student.42berlin.de>
2025-04-02 18:04:17 +0200

    server

--- server.c
+++ server.c
@@ -13,21 +13,130 @@
 #include "minitalk.h"
 
 void	sigint_handler(int sig);
+void	sigusr1_handler(int sig);
+void	sigusr2_handler(int sig);
+void	decode_char(int bit);
+void	add_char(int ch);
+
+pid_t	g_client_pid;
 
 int	main(void)
 {
 	pid_t	pid;
 
 	pid = getpid();
+	g_client_pid = 0;
 	ft_printf("Server PID: %d\n", pid);
-	signal(SIGINT, sigint_handler);
+	/*signal(SIGINT, sigint_handler);*/
+	signal(SIGUSR1, sigusr1_handler);
+	signal(SIGUSR2, sigusr2_handler);
 	while(true)
 		sleep(1);
-    return (0);
+	return (0);
 }
 
-void	sigint_handler(int sig)
+/*void	sigint_handler(int sig)
 {
 	ft_printf("CTRL + C detected, ending the proces...");
 	exit (sig);
+}*/
+void	sigusr1_handler(int sig)
+{
+	decode_char(sig * 0);
+	if (g_client_pid > 0)
+		kill(g_client_pid, SIGUSR1);
+}
+
+void	sigusr2_handler(int sig)
+{
+	decode_char(sig * 0 + 1);
+	if (g_client_pid > 0)
+		kill(g_client_pid, SIGUSR1);
+}
+
+void	decode_char(int bit)
+{
+	static int	ch;
+	static int	pos;
+
+	ch += bit << pos;
+	pos++;
+	if (pos == 8)
+	{
+		pos = 0;
+		ch = 0;
+		add_char(ch);
+	}
+}
+
+void	add_char(int ch)
+{
+	static char	*str;
+	char		*temp;
+	char		*str2;
+
+	set_to_null((void **)&temp, (void **)&str2);
+	if (!str)
+	{
+		add_first_char(str, ch);
+		return ;
+	}
+	str2 = (char *)ft_calloc(sizeof(char), 2);
+	if (!str2)
+	{
+		free((void *)str);
+		exit(-1);
+	}
+	str2[0] = ch;
+	temp = ft_strjoin(str, str2);
+	free((void *)str);
+	free((void *)str2);
+	str = temp;
+	set_to_null((void **)&temp, (void **)&str2);
+	if (ch == '\0')
+		process_str(&str);
+}
+
+void	process_str(char **str)
+{
+	static bool	flag_reading_str;
+
+	if (!flag_reading_str)
+	{
+		process_client_pid(str);
+		flag_reading_str = true;
+	}
+	else
+		print_str(str);
+}
+
+void	add_first_char(char *str, int ch)
+{
+	str = (char *)ft_calloc(sizeof(char), 2);
+	if (!str)
+		exit(-1);
+	str[0] = ch;
+}
+
+void	process_client_pid(char **str)
+{
+	g_client_pid = ft_atoi(*str);
+	free((void *)*str);
+	*str = NULL;
+	kill(g_client_pid, SIGUSR1);
+}
+
+void	print_str(char **str)
+{
+	ft_printf("%s\n", *str);
+	free((void *)*str);
+	*str = NULL;
+}
+
+void	set_to_null(void **ptr1, void **ptr2)
+{
+	if (*ptr1)
+		*ptr1 = NULL;
+	if (*ptr2)
+		*ptr2 = NULL;
 }
